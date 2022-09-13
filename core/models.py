4b2931bb984234e981d14e0a36401c1e95bd6370
Kaio <kaiosantos.infor2017@gmail.com>
2022-09-12 21:21:45 -0300

    Second

---
+++ core/models.py
@@ -0,0 +1,7 @@
+from statistics import mode
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Planta(models.Model):
+    nome = models.CharField(max_length=150)
