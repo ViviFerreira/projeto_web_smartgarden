1b079c22f79a27d571eaf18fbaa41b54feb780a1
Kaio <kaiosantos.infor2017@gmail.com>
2022-11-05 20:01:59 -0300

    authentication

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from unicodedata import name
+
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login, name='login'),
+    path('cad/', views.cad, name='cadastro'),
     path('inicio/', views.inicio, name='inicio'),
     path('areas/', views.areas, name='areas'),
     path('areas/cadastrar_areas/', views.cadastrar_areas, name='cadastrar_areas')
