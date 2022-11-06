aad177a6d7d38cbc8dccfa8dae1196f3b3576059
Kaio <kaiosantos.infor2017@gmail.com>
2022-11-06 16:54:47 -0300

    changes urls

--- core/urls.py
+++ core/urls.py
@@ -6,12 +6,14 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('cadastrar_usuarios/', views.cadastrar_usuarios, name='cadastrar_usuarios'),
-    path("logout/", views.logout, name="logout"),
+    path('cadastrar_usuarios/', views.cadastrar_usuarios,
+         name='cadastrar_usuarios'),
+    path('logout/', views.logout, name="logout"),
     path('inicio/', views.inicio, name='inicio'),
     path('areas/', views.areas, name='areas'),
-    path('areas/cadastrar_areas/', views.cadastrar_areas, name='cadastrar_areas'),
+    path('cadastrar_areas/', views.cadastrar_areas, name='cadastrar_areas'),
     path('delete_areas/<int:id>', views.delete_areas, name='delete_areas'),
-    path('plantações/', views.plantacoes, name= 'plantacoes'),
-    path('plantacoes/cadastrar_plantacoes/', views.cadastrar_plantacoes, name='cadastrar_plantacoes'),
+    path('plantacoes/', views.plantacoes, name='plantacoes'),
+    path('cadastrar_plantacoes/', views.cadastrar_plantacoes,
+         name='cadastrar_plantacoes'),
 ]
